48ddcaaf713473e75ac8466f4c901d54a7321293
colin combe <colin.combe@ed.ac.uk>
2025-02-20 17:35:00 +0100

    initial asciidoc commit with code copied from mzTab proj

--- specification_document/specdoc1_3/asciidoc/docs.sh
+++ specification_document/specdoc1_3/asciidoc/docs.sh
@@ -3,7 +3,7 @@ rm -R out/*
 asciidoctor -d book --attribute="commit-hash=COMMITHASH" --attribute="build-date=BUILDDATE" -D out ext.adoc
 asciidoctor-pdf -d book --attribute="commit-hash=COMMITHASH" --attribute="build-date=BUILDDATE" -D out ext.adoc
 html2text out/ext.html > out/ext.txt
-#asciidoctor -d book --attribute="commit-hash=COMMITHASH" --attribute="build-date=BUILDDATE" -D out main.adoc
-#head -n 6669 main.adoc > out/main_trimmed.adoc
-#asciidoctor-pdf -d book --trace --attribute="commit-hash=COMMITHASH" --attribute="build-date=BUILDDATE" -D out out/main_trimmed.adoc
+asciidoctor -d book --attribute="commit-hash=COMMITHASH" --attribute="build-date=BUILDDATE" -D out main.adoc
+head -n 6669 main.adoc > out/main_trimmed.adoc
+asciidoctor-pdf -d book --trace --attribute="commit-hash=COMMITHASH" --attribute="build-date=BUILDDATE" -D out out/main_trimmed.adoc
 
